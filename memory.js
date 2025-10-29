1c63a58b2ac1f1fb2bfaee62ae0cede68e962bbd
youcode safi <kaoutar@gmail.com>
2025-10-29 21:45:20 +0100

    creation du fonction rand qui permet de melangé les card par utilisation de function random et mathfloor et function flipcard qui permet de tourner les card par ajout du class is-flipped avec verfication

--- memory.js
+++ memory.js
@@ -20,3 +20,43 @@ triesElement.textContent = tries;
 bestScoreElement.textContent = bestScore;
 
 
+let orderRange = [...Array(cards.length).keys()];
+rand(orderRange);
+
+cards.forEach((card, index) => {
+  card.style.order = orderRange[index];
+  card.addEventListener("click", function () {
+    flipCard(card);
+  });
+});
+function rand(array) {
+  let current = array.length,
+    temp,
+    random;
+  while (current > 0) {
+    random = Math.floor(Math.random() * current);
+    current--;
+    temp = array[current];
+    array[current] = array[random];
+    array[random] = temp;
+  }
+  return array;
+}
+function flipCard(selectedCard) {
+  
+  if (selectedCard.classList.contains("is--flipped")) return;
+
+  selectedCard.classList.add("is--flipped");
+
+
+  let allFlipCards = cards.filter(card =>
+    card.classList.contains("is--flipped")
+  );
+
+  if (allFlipCards.length === 2) {
+    stopFlipping();
+    checkCards(allFlipCards[0], allFlipCards[1]);
+  }
+}
+
+
